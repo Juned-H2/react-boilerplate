4c21343b4343663cfa8a8eb216470302c888d19e
Juned Hussain <juned.hussa@outlook.com>
2021-05-12 22:52:49 +0100

    clean up

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LivecoinPro from './components/LivecoinPro';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<LivecoinPro />, document.getElementById('app'));
+ReactDOM.render(<p>Hello world!</p>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
         }]
     },
     plugins: [new HtmlWebpackPlugin ({
-        title: 'Livecoin PRO',
+        title: '',
         favicon: './assets/favicon.ico',
         template: path.join(__dirname, './src', 'index.html')
     })],
